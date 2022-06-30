229e57ca775faaf59522af5d8b9627a8d7c5455c
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-06-29 21:32:24 -0700

    commit tree 566dcacd82797af335da7d8dceeab8b92058a4c9

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (array, string) {
+    return array.filter(function (name) {
+        return string.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch (array, string) {
+    return array.filter(function (name) {
+        return name.startsWith(string);
+    })
+}
+
+const cuteDriver = [
+    {
+        name: "Cocoa Puffs",
+        hometown: "Salinas"
+    },
+    {   name: "Honey Puffs",
+        hometown: "nonexistent"
+    }
+]
+
+function matchName (array, string) {
+    return array.filter(function (driverObj) {
+        return driverObj.name === string;
+    })
+}
